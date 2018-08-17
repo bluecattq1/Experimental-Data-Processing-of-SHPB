7ef6de59eea7421728469d68c475ad7951133ede
bluecattq1 <41858782+bluecattq1@users.noreply.github.com>
2018-08-17 21:11:43 +0800

    一些功能更新
    1.更新获取文件路径的方法：os.getcwd()返回当前python的工作路径，而__file__返回当前.py文件所在的路径；这两个路径可能不同；
    2.在读取实验数据后绘制波形图输出，判断是否需要对入射杆上的信号反号；
    3.波形图增加交互功能：鼠标在曲线上时可以返回横纵坐标以及数据点的编号（从0开始），用于需要人工干预反射波和透射波区间选取的时候。

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,8 +2,8 @@
 """
 Created on Wed Jul 25 16:40:59 2018
 使用需要注意以下几点：
-1.输入数据文本格式：从哪一行开始读入；读哪几列的数据；读取的数据需不需要
-  添加负号（程序使用透射信号为正，反射信号为负）；
+1.输入数据文本格式：从哪一行开始读入；读哪几列的数据；读取的数据有没有需要
+  反号的（程序使用透射信号为正，反射信号为负）；
 2.基本参数如几何尺寸、弹性参数的更改；
 3.应力的计算公式中有面积的比，可能需要更改试样截面积的计算方式；
 4.输出截取的反射波和投射波区间，时刻注意截取的效果！特别是两端是否有因为吃波
@@ -28,26 +28,27 @@ specimenDiameter = 2.51          #mm
 areaBar = barDiameter**2 * np.pi / 4
 areaSpecimen = specimenDiameter**2
 
-currDir = os.getcwd()
+#currDir = os.getcwd()
+currDir = os.path.split(__file__)[0]
 fileName = input("Data file name is: ")
 fileAbsoluteDir = os.path.join(currDir, fileName + ".txt")
 dataStoreDir = os.path.join(currDir, fileName)
-os.mkdir(dataStoreDir)
+
+if not os.path.exists(dataStoreDir):
+  os.mkdir(dataStoreDir)
 
 shutil.copyfile(fileAbsoluteDir, os.path.join(dataStoreDir, fileName + ".txt"))
 
-time, inputBarChannel, transBarChannel = Functions.ExtractDataFromFile(
-        dataStoreDir, fileAbsoluteDir)
+time, inputBarChannel, transBarChannel = Functions.ExtractDataFromFile(dataStoreDir, fileAbsoluteDir)
 
-#flag = input("Needed to make reflect wave negative?(Y/N)")
-#if flag == "Y":
-#    inputBarChannel = [-temp for temp in inputBarChannel]
+flag = input("Needing to reverse input bar wave?(Y/N)")
+if flag == "Y":
+    inputBarChannel = [-temp for temp in inputBarChannel]
+    Functions.DrawChannelSignalsPlot(dataStoreDir, "Original Experimental Data.png", time, inputBarChannel, transBarChannel)
 
-inputChannel, transChannel = Functions.MovingChannelSignals(
-        dataStoreDir, time, inputBarChannel, transBarChannel)
+inputChannel, transChannel = Functions.MovingChannelSignals(dataStoreDir, time, inputBarChannel, transBarChannel)
 
-timeModified, reflectWave, transWave = Functions.FindingSections(
-        dataStoreDir, time, inputChannel, transChannel)
+timeModified, reflectWave, transWave = Functions.FindingSections(dataStoreDir, time, inputChannel, transChannel)
 
 engStress, engStrain, engStrainRate = Functions.Calculations(timeModified, 
                 reflectWave, transWave, K, barWaveSpeed, barElasticModulus, 
@@ -58,10 +59,10 @@ trueStress, trueStrainRate, trueStrain = \
 
 Functions.DrawStressStrainPlot(dataStoreDir, engStress, engStrain)
 
-kTrue, b = Functions.FittingStrainRate(dataStoreDir, timeModified, trueStrain)
+kEng, b = Functions.FittingStrainRate(dataStoreDir, timeModified, engStrain, False)
+kTrue, b = Functions.FittingStrainRate(dataStoreDir, timeModified, trueStrain, True)
 
 print("Strain rate is: ", kTrue)
 
-Functions.WritingDataToExcel(dataStoreDir, timeModified, engStress, \
-                             engStrain, engStrainRate, trueStress, \
+Functions.WritingDataToExcel(dataStoreDir, timeModified, engStress, engStrain, engStrainRate, trueStress, \
                              trueStrain, trueStrainRate, kTrue, kEng)
